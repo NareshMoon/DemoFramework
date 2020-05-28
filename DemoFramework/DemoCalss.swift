314220e0f7809b70dc0fa19442e3153b6a02be69
Itharaju, Naresh (Contractor) <Naresh.Itharaju@CVSHealth.com>
2020-05-28 13:34:37 -0400

    initial setup

---
+++ DemoFramework/DemoCalss.swift
@@ -0,0 +1,15 @@
+//
+//  DemoCalss.swift
+//  DemoFramework
+//
+//  Created by Itharaju, Naresh (Contractor) on 5/28/20.
+//  Copyright © 2020 Itharaju, Naresh (Contractor). All rights reserved.
+//
+
+import Foundation
+
+public class DemoClass {
+    public func printValue(_ str: String) {
+        print(str)
+    }
+}
